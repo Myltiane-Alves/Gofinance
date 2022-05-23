ca9055159bea8c11ffb52d2a29630655a80f9dad
Myltiane Alves <myltiane.alves@cleandev.com.br>
2022-05-23 17:43:43 -0300

    finalizando

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+import { useTheme } from 'styled-components';
+import { Platform } from 'react-native';
+
+import { Dashboard } from '../screens/Dashboard';
+import { Register } from '../screens/Register';
+import { Resume } from '../screens/Resume';
+
+export function AppRoutes() {
+  const theme = useTheme();
+
+  return (
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarHideOnKeyboard: true,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 70,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0
+        }
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons
+              name="attach-money"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Resume}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons
+              name="pie-chart"
+              size={size}
+              color={color}
+            />
+          )
+        }}
+      />
+    </Navigator>
+  )
+}
